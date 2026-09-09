8f1fec6a011d7c6eba449669a1d6ac66be1fd429
rlatnfla <rlatnfla0724@gmail.com>
2026-09-09 23:32:50 +0900

    refactor: MyBatis Mapper 중복 쿼리 간편화 및 파라미터 적용

--- src/main/java/com/rlatnfla/demo1/coremusic/infrastructure/mybatis/mapper/AlbumMyBatisMapper.java
+++ src/main/java/com/rlatnfla/demo1/coremusic/infrastructure/mybatis/mapper/AlbumMyBatisMapper.java
@@ -4,20 +4,20 @@ import com.rlatnfla.demo1.coremusic.infrastructure.mybatis.entity.AlbumMyBatisEn
 import java.util.List;
 import java.util.Optional;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface AlbumMyBatisMapper {
 
-    void insert(AlbumMyBatisEntity entity);
+    void insert(@Param("entity") AlbumMyBatisEntity entity);
 
-    void update(AlbumMyBatisEntity entity);
+    void update(@Param("entity") AlbumMyBatisEntity entity);
 
     Optional<AlbumMyBatisEntity> findById(Long id);
 
     List<AlbumMyBatisEntity> findAllByTitle(String title);
 
     List<AlbumMyBatisEntity> findAllByArtistId(Long id);
-
 
 
 }
